fbb3eef0b2c862b8af1dd6a5f5d8b5246de65982
Francinildo M. Rodrigues <francis584@gmail.com>
2020-07-24 12:42:56 -0300

    learning TypeScript. fix

--- src/routes.ts
+++ src/routes.ts
@@ -3,7 +3,7 @@ import createUser from "./services/CreateUser";
 
 export function helloWorld(request: Request, response: Response) {
   const user = createUser({
-    email: "francis@gmail.com",
+    email: "francis@teste.com",
     password: "13-14-83-",
     name: "Francinildo",
     techs: [
